36bfcdc2aa59af3f7ca71d530a5eccfd8aeec4b2
Nathan Otto <nathan@Nathans-MacBook-Pro.local>
2016-03-16 00:45:56 -0700

    add stuff

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,18 @@ var uglify = require('gulp-uglify');
 var utilities = require('gulp-util');
 var del = require("del");
 var jshint = require("gulp-jshint");
-var lib = require('bower-files')();
+var browserSync = require("browser-sync").create();
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap":{
+      "main":[
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
 var buildProduction = utilities.env.production;
 
 
@@ -35,6 +46,7 @@ gulp.task("build", ["clean"], function(){
   } else {
     gulp.start("jsBrowserify");
   }
+  gulp.start('bower');
 });
 gulp.task("jshint", function(){
   return gulp.src(["js/*.js"])
@@ -46,4 +58,26 @@ gulp.task('bowerJS', function(){
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
+});
+gulp.task('bowerCSS', function(){
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+gulp.task('serve', function(){
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
 });
